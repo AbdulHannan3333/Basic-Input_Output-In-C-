5e183db67fc68021a0d0875963cccd1f2338a502
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-16 01:27:15 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Input_Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Your Name is {name}");
+        }
+    }
+}
